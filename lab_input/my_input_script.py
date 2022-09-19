64e2106b44ba798a6bd75e4f38a60f5cdd1d13f4
chrogan <chrogan@users.noreply.github.com>
2022-09-19 21:37:05 +0000

    challenge for input

---
+++ lab_input/my_input_script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This was a challenge for SHEBAND, input, print,concatenate, and variables"""
+
+
+def main():
+    user_name = input("What is your name?")
+    day_of_week = input("Which day of the week is it?")
+
+    print("Hello,",user_name+"!","Happy",day_of_week+"!")
+
+
+main()
